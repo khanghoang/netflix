d0fd505b837ac356ac0382c3e18fec14555d3b17
Khang Hoang <hoangtrieukhang@gmail.com>
2016-12-02 01:32:46 +0700

    work on movie cell

---
+++ app.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import MovieCell from './src/components/MovieCell';
+
+export default class netflix extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+	<MovieCell />
+	<MovieCell />
+	<MovieCell />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 7,
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ src/components/MovieCell.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+
+export default () => (
+  <TouchableOpacity
+    onPress={() => {}}
+  >
+    <Image
+      style={{
+        width: 115,
+        height: 165,
+      }}
+      source={{
+        uri: 'https://image.tmdb.org/t/p/w500/e1mjopzAS2KNsvpbpahQ1a6SkSn.jpg',
+      }}
+    />
+  </TouchableOpacity>
+);
