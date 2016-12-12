42cb6b8d66f1b7131164fa46bb106ff95c8e4138
Khang Hoang <hoangtrieukhang@gmail.com>
2016-12-12 22:54:17 +0700

    finish list of seasons

--- src/components/Player/Series.js
+++ src/components/Player/Series.js
@@ -1,9 +1,63 @@
 import React, { Component } from 'react';
-import {
-  Dimensions,
-  View, Text, TouchableOpacity } from 'react-native';
+import { ScrollView, Dimensions, View, Text, TouchableOpacity } from 'react-native';
+import { compose } from 'recompose';
+import { connect } from 'react-redux';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const { width, height } = Dimensions.get('window');
+
+const CloseButton = ({ onPress }) => (
+  <TouchableOpacity
+    onPress={onPress}
+  >
+    <Icon name="ios-close" size={40} color="white" />
+  </TouchableOpacity>
+);
+
+const EnhancedCloseButton = compose(
+  connect(
+    null,
+    null,
+  )
+)(CloseButton);
+
+const SeasonCell = ({ title, isSelected }) => (
+  <TouchableOpacity
+    style={{
+      paddingHorizontal: 20,
+    }}
+  >
+    <View
+      style={{
+        borderTopWidth: 4,
+        borderTopColor: isSelected ? '#C80814' : 'transparent',
+      }}
+    >
+      <Text
+        style={{
+          paddingTop: 10,
+          fontSize: 16,
+          color: '#E5E6E7',
+          fontWeight: isSelected ? '600' : '300',
+        }}
+      >
+        {title}
+      </Text>
+    </View>
+  </TouchableOpacity>
+);
+
+const ListSeries = ({ listSeries }) => {
+  const seriesCells = listSeries.map(series => <SeasonCell isSelected title={series} />);
+  return (
+    <ScrollView
+      style={{ flex: 1, height: 60, backgroundColor: 'transparent', }}
+      horizontal
+    >
+      {seriesCells}
+    </ScrollView>
+  );
+}
 
 const Series = () => (
   <View
@@ -18,6 +72,12 @@ const Series = () => (
       backgroundColor: '#131415',
     }}
   >
+    <View
+      style={{ flex: 1, flexDirection: 'row', }}
+    >
+      <ListSeries listSeries={['SEASON 1', 'SEASON 2', 'SEASON 3']} />
+      <EnhancedCloseButton />
+    </View>
   </View>
 );
 
